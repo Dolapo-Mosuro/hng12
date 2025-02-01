583920745a00985faf23f3caa3f430bacd239f1f
dolapo-mosuro <mosurodolapo@gmail.com>
2025-02-01 01:00:02 +0100

    Deploy API to Vercel

--- public-api/index.js
+++ public-api/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.json({
+	res.status(200).json({
 		email: process.env.EMAIL,
 		current_datetime: new Date().toISOString(),
 		github_url: process.env.GITHUB_URL,
